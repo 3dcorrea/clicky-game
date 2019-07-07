3182a3dc30dcee50784fb6181c1655ae25eb536e
Dan Correa <dancorrea3@gmail.com>
2019-07-06 21:33:34 -0400

    renamed files

--- clicky-game0/src/App.js
+++ clicky-game0/src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import FriendCard from "./components/FriendCard";
+import Picture from "./components/Picture";
 import friends from "./friends.json";
 
 function App() {
   return (
     <Wrapper>
       <Title>Friends List</Title>
-      <FriendCard
+      <Picture
         name={friends[0].name}
         image={friends[0].image}
         occupation={friends[0].occupation}
         location={friends[0].location}
       />
-      <FriendCard
+      <Picture
         name={friends[1].name}
         image={friends[1].image}
         occupation={friends[1].occupation}
         location={friends[1].location}
       />
-      <FriendCard
+      <Picture
         name={friends[2].name}
         image={friends[2].image}
         occupation={friends[2].occupation}
